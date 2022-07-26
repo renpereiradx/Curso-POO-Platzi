961b6165c1f0b05e0be413fd308a14631b5ea756
renpereiradx <renedx92@gmail.com>
2022-07-26 10:13:04 -0400

    Constructores declarados

--- Curso-POO-Uber/JS/Car.js
+++ Curso-POO-Uber/JS/Car.js
@@ -11,6 +11,7 @@ class Car {
         console.log(this.driver)
         console.log(this.driver.name)
         console.log(this.driver.document)
+        console.log(this.passenger)
     }
 }
 
--- Curso-POO-Uber/JS/index.js
+++ Curso-POO-Uber/JS/index.js
@@ -1,3 +1,7 @@
 var car = new Car("FJE392", new Account("Jose Carlos", "7.584.678"))
 car.passenger = 4;
 car.printDataCar()
+
+var uberX = new UberX("HPC427", new Account("Luis Carrasco", "DLH483"), "Chevrolet", "Spark")
+uberX.passenger = 4
+uberX.printDataCar()
