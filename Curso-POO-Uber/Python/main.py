dee34cfbd0c73eed168a036ba41851d1a77e26b4
renpereiradx <renedx92@gmail.com>
2022-07-26 10:11:52 -0400

    Constructores declarados

--- Curso-POO-Uber/Python/main.py
+++ Curso-POO-Uber/Python/main.py
@@ -1,7 +1,10 @@
 from account import Account
 from car import Car
+from uberX import UberX
 
 if __name__ == "__main__":
     print("Hola Mundo")
     car = Car("RMK167", Account("Gustavito", "5.731.478"))
     print(vars(car.driver))
+    uberX = UberX("DLE943", Account("Andres Peralta", "3.846.157"), "Toyota", "Corolla")
+    print(vars(uberX.driver))
